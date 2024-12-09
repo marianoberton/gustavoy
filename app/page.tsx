29730743dcf68806cadf385765ea24592335954c
Mariano <marianoberton@gmail.com>
2024-12-09 13:13:51 -0300

    agregado banner

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,48 @@ import Image from "next/image";
 
 const Sidebar = () => {
   return (
-    <aside className="w-full lg:w-1/4 bg-white shadow-md rounded-lg p-4">
-      <h3 className="text-lg font-bold mb-4">Últimos Tweets</h3>
-      <div className="overflow-hidden">
+    <aside className="w-full lg:w-1/4 bg-white shadow-md rounded-lg p-4 space-y-8">
+      {/* Feed de Twitter */}
+      <div>
+        <h3 className="text-lg font-bold mb-4">Últimos Tweets</h3>
+        <div className="overflow-hidden">
+          <a
+            className="twitter-timeline"
+            href="https://twitter.com/GustavoYarroch?ref_src=twsrc%5Etfw"
+            data-height="1700"
+          >
+            Tweets by GustavoYarroch
+          </a>
+          <script
+            async
+            src="https://platform.twitter.com/widgets.js"
+            charSet="utf-8"
+          ></script>
+        </div>
+      </div>
+
+      {/* Banner debajo del feed de Twitter */}
+      <div className="mt-8">
         <a
-          className="twitter-timeline"
-          href="https://twitter.com/GustavoYarroch?ref_src=twsrc%5Etfw"
-          data-height="2000" // Aumenta la altura aquí
+          href="https://www.legislatura.gob.ar"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block"
         >
-          Tweets by GustavoYarroch
+          <Image
+            src="/banner.jpeg" // Asegúrate de que la imagen esté en la carpeta `public/`
+            alt="Legislatura de Buenos Aires"
+            width={300}
+            height={250}
+            className="mx-auto"
+          />
         </a>
       </div>
-      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
     </aside>
   );
 };
+
+
 
 
 export default async function Home() {
