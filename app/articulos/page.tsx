7c70cda9b0a61d8ac1e98131aa80e17432e04627
Mariano <marianoberton@gmail.com>
2024-12-09 21:02:12 -0300

    agregadas 6 notas

--- app/articulos/page.tsx
+++ app/articulos/page.tsx
@@ -81,6 +81,72 @@ export default function ArticulosPage() {
       articleUrl:
         "https://www.infobae.com/deportes/2024/10/11/los-cambios-que-podria-realizar-lionel-scaloni-en-la-seleccion-argentina-para-el-duelo-ante-bolivia-del-proximo-martes/",
     },
+    {
+      id: 7,
+      title:
+        "Nicolás Paz, otro de los “europibes” que seducen a Scaloni: su historia y la influencia del alemán Kroos",
+      excerpt:
+        "El mediocampista ofensivo es uno de los juveniles citados por el técnico campeón del mundo para los partidos frente a Venezuela y Bolivia. Hoy irá al banco de suplentes en Maturín.",
+      imageUrl:
+        "https://www.infobae.com/resizer/v2/X72QY7H77VA6DE75DD2RTND64Q.jpg?auth=ae840fb2f61a5cceba0a2bfb9a93c9f1fcb0b962fff7c946f6db0be8e6b14031&smart=true&width=400&height=225&quality=85",
+      articleUrl:
+        "https://www.infobae.com/deportes/2024/10/10/nicolas-paz-otro-de-los-europibes-que-seducen-a-scaloni-su-historia-y-la-influencia-del-aleman-kroos/",
+    },
+    {
+      id: 8,
+      title:
+        "Por qué recuperar la mejor versión de Borja es otro de los nuevos objetivos de Gallardo en River Plate",
+      excerpt:
+        "El colombiano rendía mejor con Martín Demichelis que ahora con el “Muñeco”, quien fue el que pidió insistentemente por su llegada al conjunto “millonario”.",
+      imageUrl:
+        "https://www.infobae.com/resizer/v2/J54YPANWVNHUHPEJY6RGYV4EEM.jpg?auth=0761b81beaaa01985574ec13954c05c586e41f16bb651980b7f67d929983cff0&smart=true&width=400&height=225&quality=85",
+      articleUrl:
+        "https://www.infobae.com/deportes/2024/09/29/por-que-recuperar-la-mejor-version-de-borja-es-otro-de-los-nuevos-objetivos-de-gallardo-en-river-plate/",
+    },
+    {
+      id: 9,
+      title:
+        "Los cuatro grandes ganadores de River tras el triunfo contra Boca y el “premio” frente a Colo Colo",
+      excerpt:
+        "El clásico frente al Xeneize dejó un semblante positivo en varios protagonistas, que quieren estar en la revancha de los cuartos de final de la Copa Libertadores.",
+      imageUrl:
+        "https://www.infobae.com/resizer/v2/Q33UKND2NNEXNIOLGESRDNYRJM.jpg?auth=4264d1e9677ae99dcc6e4bd00ea9aeebf57c85c9ed639cae53c807db210ed1b2&smart=true&width=400&height=225&quality=85",
+      articleUrl:
+        "https://www.infobae.com/deportes/2024/09/24/los-cuatro-grandes-ganadores-de-river-tras-el-triunfo-contra-boca-y-el-premio-frente-a-colo-colo/",
+    },
+    {
+      id: 10,
+      title:
+        "Convicción, triunfos resonantes y sorpresas tácticas: los Superclásicos de Marcelo Gallardo y cómo los vive",
+      excerpt:
+        "El técnico de River Plate modificó la historia moderna del club con las cinco victorias consecutivas en partidos de eliminación directa o finales, entre 2014 y 2019.",
+      imageUrl:
+        "https://www.infobae.com/resizer/v2/PRFAANS2RFDXDI7A3Y5TCMPWW4.JPG?auth=e281237f057675b9ec35ae156d0992ca4a5577c0c54f90d7f5b3a3e68381890b&smart=true&width=400&height=225&quality=85",
+      articleUrl:
+        "https://www.infobae.com/deportes/2024/09/21/conviccion-triunfos-resonantes-y-sorpresas-tacticas-los-superclasicos-de-marcelo-gallardo-y-como-los-vive/",
+    },
+    {
+      id: 11,
+      title:
+        "Del hito de Scaloni a la solidez defensiva: las cinco claves de la Selección de cara al duro partido frente a Colombia",
+      excerpt:
+        "El equipo campeón del mundo y bicampeón de América afrontará esta tarde un exigente partido en Barranquilla frente al conjunto dirigido por Néstor Lorenzo.",
+      imageUrl:
+        "https://www.infobae.com/resizer/v2/EHVUVN7BFNE6HE6BON4DBVSZS4?auth=18283d3e1aeb8b0fb855318ad14823606f6ff9f54608436bcb2fe584ecf58cf9&smart=true&width=400&height=225&quality=85",
+      articleUrl:
+        "https://www.infobae.com/deportes/2024/09/10/del-hito-de-scaloni-a-la-solidez-defensiva-las-cinco-claves-de-la-seleccion-de-cara-al-duro-partido-frente-a-colombia/",
+    },
+    {
+      id: 12,
+      title:
+        "Todos los detalles de la despedida a Ángel Di María durante el partido de la selección argentina en el Monumental",
+      excerpt:
+        "Fideo estará presente en el duelo ante Chile por las Eliminatorias, pero ya no como jugador. Se espera una noche especial para uno de los héroes de La Scaloneta.",
+      imageUrl:
+        "https://www.infobae.com/resizer/v2/WTPNKMS3MZADLCN6H2YQUWZACE?auth=bb8f7bc13c453939a33c347102bd2c58a42e03284347fce1e8ca3b1d4b8c6ea4&smart=true&width=400&height=225&quality=85",
+      articleUrl:
+        "https://www.infobae.com/deportes/2024/09/10/del-hito-de-scaloni-a-la-solidez-defensiva-las-cinco-claves-de-la-seleccion-de-cara-al-duro-partido-frente-a-colombia/",
+    },
   ];
 
   return (
