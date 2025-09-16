8931ab1020d41989208de7bf446d135862fbe727
Mariano Berton <marianoberton@gmail.com>
2025-09-16 15:37:29 -0300

    agosto sovled

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,26 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http", // Cambiado de https a http
+        protocol: "http",
         hostname: process.env.WORDPRESS_HOSTNAME || "l0060181.ferozo.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.infobae.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
         port: "",
         pathname: "/**",
       },
